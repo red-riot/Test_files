ab1cb88f9deed2e4ff9131dc0c5fbc906bd51e06
Agata <48286659+red-riot@users.noreply.github.com>
2019-09-30 10:21:32 +0200

    Update manamana.py

--- manamana.py
+++ manamana.py
@@ -2,6 +2,9 @@ def main():
   print('efnejrkhugerihguevnjfdjkjkemfkwe')
   
   #let's see if anything changes :3c
+  
+  # here here
+  # sum new changers >:3ccc
 
 if __name__ == '__main__':
   main()
